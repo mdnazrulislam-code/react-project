23b84e52d12c01a77cd8647bebda72be46980e3d
mdnazrulislam-code <arfanislamabir@gmail.com>
2024-04-05 22:19:16 +0600

    react router added

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './components/Home/Home'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   
+    </>
+  )
+}
+
+export default App
